c2e7b67771bf1590e1a5be614ff7b01b88fd1396
Jaime Gonzalez Bonorino <jaimegbonorino@gmail.com>
2017-08-28 17:12:05 -0300

    Fixing inbounds and onclick methods

--- Engine/GuiComponent.h
+++ Engine/GuiComponent.h
@@ -25,18 +25,8 @@ class GuiComponent {
 		void setTipo(enumTipo TIPO){ tipo = TIPO; };
 		enumTipo getTipo(){ return tipo;};
 
-		bool inBounds(int X, int Y){
-			if( X >= x && X <= x+(texto.size()*TEXTO_SIZE)){
-				if( Y >= y && Y <= y+(texto.size())){
-				return true;
-				}
-			}else return false;
-				
-		};
-
-		void onClick(){
-			std::cout<<texto<<std::endl;
-		}
+		virtual bool inBounds(int,int)  = 0;
+		virtual onClick() = 0;
 
 		
 
@@ -52,6 +42,18 @@ class GuiTexto : public GuiComponent {
 			GuiComponent::setTipo(TEXTO);
 		};
 		~GuiTexto(){};
+		virtual bool inBounds(int X, int Y){
+			if( X >= GuiComponent::getX() && X <= GuiComponent::getX()+(texto.size()*TEXTO_SIZE)){
+				if( Y >= GuiComponent::getY() && Y <= GuiComponent::getY()+(GuiComponent::getText().size())){
+				return true;
+				}
+			}else return false;
+				
+		};
+
+		virtual void onClick(){
+			std::cout<<GuiComponent::getText()<<std::endl;
+		}
 };
 
 
@@ -60,18 +62,26 @@ class GuiButton : public GuiComponent {
 		GuiButton(std::string txt, int X, int Y):GuiComponent(txt,X,Y){
 			GuiComponent::setTipo(BUTTON);
 			sprite = new Sprite();
-			sprite->agregarFrame(0,0,64,32);
+			sprite->agregarFrame(TILE_W*7,0,64,32);
+			sprite->agregarFrame(TILE_W*9,0,64,32);
 		};
 		~GuiButton(){};
 
 		/*Override*/
-		bool inBounds(int X, int Y){
+		virtual bool inBounds(int X, int Y){
 			if(X >= GuiComponent::getX() && X <= GuiComponent::getX()+sprite->getFrame()->w){
 				if(Y >= GuiComponent::getY() && Y <= GuiComponent::getY()+sprite->getFrame()->h){
+					std::cout<< Y << " - " << GuiComponent::getY() << " - " << sprite->getFrame()->h<<std::endl;
 					return true;
 				}
 			}
 			else return false;
+		};
+		
+		/* Override */
+		virtual void onClick(){
+			sprite->play();
+			sprite->run();
 		};
 
 		Sprite * sprite;
